b30213e0c4ee7f938102dda14a50ad6a8f31aa7e
sora082011zero-alt <sora082011zero@gmail.com>
2026-06-17 22:51:53 +0900

    repository ok

--- src/main/java/com/sora/Sora_index/Entity/SectorHistory.java
+++ src/main/java/com/sora/Sora_index/Entity/SectorHistory.java
@@ -1,8 +1,29 @@
 package com.sora.Sora_index.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import java.time.LocalDate;
 
+@Getter
+@Setter
 @Entity
+@Table(name = "sector_history")
 public class SectorHistory {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate tradeDate;
+
+    private String sectorName;
+
+    private Integer rankNo;
+
+    private Integer score;
+
+    private Double changeRate;
+
+    // getter setter
 }
---
+++ src/main/java/com/sora/Sora_index/Repository/SectorHistoryRepository.java
@@ -0,0 +1,9 @@
+package com.sora.Sora_index.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.sora.Sora_index.Entity.SectorHistory;
+
+public interface SectorHistoryRepository
+        extends JpaRepository<SectorHistory, Long> {
+
+}
